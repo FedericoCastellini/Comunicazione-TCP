32dd7945bbab9716b1a9897ecd4269d695931963
FedericoCastellini <92482037+FedericoCastellini@users.noreply.github.com>
2022-04-08 14:50:58 +0200

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,24 @@
+package connessionetcp;
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    public static void main(String[] args) {
+        int port = 2000;
+        
+        Socket socket = null;
+        try{
+            ServerSocket sSocket = new ServerSocket(port);
+            System.out.println("In attesa di connessione con il client");
+            
+            socket = sSocket.accept();
+            System.out.println("Server connesso con il socket: "+ socket.getRemoteSocketAddress());
+            
+            socket.getInputStream();
+        }catch(BindException be) {
+            System.err.println("Server già avviato");
+        }catch(IOException ioe){
+            System.err.println("Errore I/O nell'instanza del server");
+        }
+    }
+}
